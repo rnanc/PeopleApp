4b8036e1712dcf75b214095fd89aec311f424c7b
rnanc <renanfigcarneiro@gmail.com>
2019-09-18 16:27:07 -0300

    initial

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header'
+import axios from 'axios';
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      people :[]
+    };
+  } 
+
+  componentDidMount(){
+    axios.get('https://randomuser.me/api/?nat=br&results=5').then(response =>{
+      const{results} = response.data;
+      this.setState({
+        people: results
+      })
+    })
+  }
+
+  renderList(){
+    const textElements = this.state.people.map(people => {
+      return <Text key={people.name.first} >{people.name.first}</Text>
+    });
+
+    return textElements;
+  };
+
+  render(){
+    return (
+      <View>
+        <Header title="PeopleApp" />
+        {this.renderList()}
+      </View>
+    );
+  } 
+};
+
